5cb879b52531daa2b0e5c64f15a0e1355bd35ca4
Thi Le <thi.le@unc.edu>
2022-07-17 18:51:51 -0700

    Initial commit

--- Prework/ViewController.swift
+++ Prework/ViewController.swift
@@ -9,11 +9,24 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var billAmountTextField: UITextField!
+    @IBOutlet weak var tipAmountLabel: UILabel!
+    @IBOutlet weak var totalLabel: UILabel!
+    @IBOutlet weak var tipControl: UISlider!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func calculateTipSlider(_ sender: Any) {
+        let bill = Double(billAmountTextField.text!) ?? 0
+        
+        let tip = Double(tipControl.value)
+        
+        let total = bill + (bill * (tip / 100))
+        tipAmountLabel.text = String(format: "%.2f%%", tip)
+        totalLabel.text = String(format: "$%.2f", total)
+    }
+    
 }
 
